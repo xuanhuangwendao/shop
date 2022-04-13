3dc4c1145b55ee017210958e85b3bc8561bbbdba
xuanhuang <xuanhuangwendao@gmail.com>
2022-04-14 01:03:44 +0800

    重构

--- src/main/java/com/xing/shop/domain/response/UploadFileResponse.java
+++ src/main/java/com/xing/shop/domain/response/UploadFileResponse.java
@@ -1,9 +1,12 @@
 package com.xing.shop.domain.response;
 
+import lombok.Data;
+
 /**
  * @author ：xuanhuangwendao
  * @date ：Created in 2022/4/14 0:48
  */
+@Data
 public class UploadFileResponse {
 
     private boolean success = true;
